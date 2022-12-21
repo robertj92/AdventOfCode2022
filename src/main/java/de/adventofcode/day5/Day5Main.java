223b89a26d4440b094dacea7c35739fdd6d5ea6a
Robert Junklewitz <robert.junklewitz@googlemail.com>
2022-12-21 13:31:54 +0100

    finish day 5

--- src/main/java/de/adventofcode/day5/Day5Main.java
+++ src/main/java/de/adventofcode/day5/Day5Main.java
@@ -1,8 +1,13 @@
 package de.adventofcode.day5;
 
+import de.adventofcode.utils.FileUtils;
+
 public class Day5Main {
 
     public static void main(String[] args) {
-
+        StackCrateOrganizer stackCrateOrganizer = new StackCrateOrganizer();
+        String input = FileUtils.readFileFromString("day5/input.txt");
+        String organizedTopCrates = stackCrateOrganizer.getOrganizedTopCrates(input);
+        System.out.println("The organized top crates are " + organizedTopCrates);
     }
 }
--- src/main/java/de/adventofcode/day5/StackCrateOrganizer.java
+++ src/main/java/de/adventofcode/day5/StackCrateOrganizer.java
@@ -3,30 +3,56 @@ package de.adventofcode.day5;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
-import java.util.stream.Collectors;
 
 public class StackCrateOrganizer {
 
     public static final Integer STACK_LENGTH = 4;
 
-    /*
-    [A] [B]
-    1   2
-
-    move 1 from 1 to 2
-     */
-
     public String getOrganizedTopCrates(String input) {
-        String crates = "A";
-
         int numberOfStacks = getNumberOfStacks(input);
         List<Stack<String>> stacks = initializeStacks(numberOfStacks);
 
         fillStacks(input, stacks);
 
-        return crates;
+        moveCratesInStacks(input, stacks);
+
+        return getTopCratesFromStacks(stacks);
+    }
+
+    private String getTopCratesFromStacks(List<Stack<String>> stacks) {
+        StringBuilder result = new StringBuilder();
+
+        for (Stack<String> stack : stacks) {
+            if (!stack.empty()) {
+                result.append(stack.peek());
+            }
+        }
+
+        return result.toString();
+    }
+
+    private void moveCratesInStacks(String input, List<Stack<String>> stacks) {
+        input.lines().filter(line -> line.startsWith("move")).forEach(line -> {
+            String[] lineElements = line.split(" ");
+            int numberOfCrates = Integer.parseInt(lineElements[1]);
+            int fromCrate = Integer.parseInt(lineElements[3]);
+            int toCrate = Integer.parseInt(lineElements[5]);
+
+            Stack<String> cratesToMove = new Stack<>();
+
+            while (numberOfCrates > 0) {
+                String crate = stacks.get(fromCrate - 1).pop();
+                cratesToMove.push(crate);
+                numberOfCrates--;
+            }
+
+            while (!cratesToMove.empty()) {
+                stacks.get(toCrate - 1).push(cratesToMove.pop());
+            }
+        });
     }
 
     private void fillStacks(String input, List<Stack<String>> stacks) {
@@ -34,16 +60,23 @@ public class StackCrateOrganizer {
                 .filter(line -> line.contains("["))
                 .toList();
 
-        stackRows.forEach(stackRow -> {
-            for (int rowIndex = 1; rowIndex < stacks.size() * STACK_LENGTH; rowIndex += STACK_LENGTH) {
+        stackRows = new ArrayList<>(stackRows);
 
+        Collections.reverse(stackRows);
+
+        stackRows.forEach(stackRow -> {
+            for (int rowIndex = 1; rowIndex < stacks.size() * STACK_LENGTH && rowIndex < stackRow.length(); rowIndex += STACK_LENGTH) {
+                char characterAtPosition = stackRow.charAt(rowIndex);
+                if (characterAtPosition != ' ') {
+                    stacks.get(rowIndex / STACK_LENGTH).push(String.valueOf(characterAtPosition));
+                }
             }
         });
     }
 
     private int getNumberOfStacks(String input) {
         String stackNumberingRow = input.lines()
-                .filter(line -> !line.isBlank() && StringUtils.isNumeric(line.substring(0, 1)))
+                .filter(line -> !line.isBlank() && StringUtils.isNumeric(line.substring(1, 2)))
                 .findFirst()
                 .orElseThrow();
 
